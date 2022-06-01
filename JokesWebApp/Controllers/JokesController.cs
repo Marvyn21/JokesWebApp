0fd16efcad612ed8caa6b4c6aa7dec8a8c1641d3
Marvyn21 <marvynrogony@gmail.com>
2022-06-01 11:54:10 +0300

    modified changes

--- JokesWebApp/Controllers/JokesController.cs
+++ JokesWebApp/Controllers/JokesController.cs
@@ -25,6 +25,18 @@ namespace JokesWebApp.Controllers
               return View(await _context.Joke.ToListAsync());
         }
 
+        // GET: ShowSearchForm
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            return View();
+        }
+
+        // POST: ShowSearchResults
+        public async Task<IActionResult> ShowSearchResults(string SearchPhrase)
+        {
+            return View("Index", await _context.Joke.Where(j => j.JokeQuestion.Contains(SearchPhrase)).ToListAsync());
+        }
+
         // GET: Jokes/Details/5
         public async Task<IActionResult> Details(int? id)
         {
